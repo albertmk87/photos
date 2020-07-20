eb061873d60aa5f08c032caf17badc3c0f0f3c08
Albert Miftar <a.miftar@aa.mk>
2020-07-20 13:50:01 +0200

    Updates

---
+++ precache-manifest.d9b3f33e2ed5d8a5cbbe9bb01d1fccba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc561a904cfc81a32d55f5edfa4e3eda",
+    "url": "/photos/index.html"
+  },
+  {
+    "revision": "c0579ad14b0dad9119b8",
+    "url": "/photos/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "95c2bc5a133b8aedec02",
+    "url": "/photos/static/css/main.76caa5b3.chunk.css"
+  },
+  {
+    "revision": "c0579ad14b0dad9119b8",
+    "url": "/photos/static/js/2.935f16cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/photos/static/js/2.935f16cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c2bc5a133b8aedec02",
+    "url": "/photos/static/js/main.dcd22032.chunk.js"
+  },
+  {
+    "revision": "2d2a5655bcaac5e18d50",
+    "url": "/photos/static/js/runtime-main.a6c9132d.js"
+  }
+]);
